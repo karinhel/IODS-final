a15a6c5dbe7bf729009064e7921c1cfa3a18000b
karinhel <karin.a.helander@helsinki.fi>
2017-02-26 11:19:53 +0200

    getting started!

---
+++ wranling_final.R
@@ -0,0 +1,17 @@
+#####################################################################################################
+
+###### FINAL ASSIGNMENT #########
+
+# Data wranling
+# Karin Helander
+# Date: 25.02.2017
+
+# Let's wrangle the previously introduced 'Tea' data from the package FactoMineR.
+# After the wranling is finished, the data will be used for logistic regression analysis
+
+
+library(FactoMineR)
+
+data("tea")
+
+str(tea)
